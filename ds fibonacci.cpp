d2a8f7b811ee12dbe9ca36e007a9485a9c7dfaee
naveenmohan289-hash <naveenmohan289@gmail.com>
2025-08-23 17:39:25 +0530

    Add files via upload

---
+++ ds fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Recursive function to return nth Fibonacci number
+int fibonacci(int n) {
+    if (n == 0)      // Base case
+        return 0;
+    else if (n == 1) // Base case
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n, i;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
+    return 0;
+}
